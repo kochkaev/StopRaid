5ef14e0a6a9de0d885722f242305a3a87ffd3d6c
kochkaev <edukisel@yandex.ru>
2025-03-13 15:36:14 +0700

    Added /stopraid command

--- src/main/kotlin/ru/kochkaev/stopraid/StopRaid.kt
+++ src/main/kotlin/ru/kochkaev/stopraid/StopRaid.kt
@@ -1,9 +1,41 @@
 package ru.kochkaev.stopraid
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
+import net.minecraft.server.command.CommandManager
+import net.minecraft.text.Style
+import net.minecraft.text.Text
+import net.minecraft.text.TextColor
+import net.minecraft.util.Colors
 
 class StopRaid : ModInitializer {
 
     override fun onInitialize() {
+        CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
+            dispatcher.register(
+                CommandManager.literal("stopraid")
+                    .executes {
+                        val player = it.source.player
+                        if (player!=null) {
+                            val raid = player.serverWorld.getRaidAt(player.blockPos)
+                            if (raid != null) {
+                                raid.invalidate()
+                                it.source.sendFeedback({
+                                    Text.of("Рейд был принудительно остановлен.")
+                                        .getWithStyle(Style.EMPTY.withColor(Colors.LIGHT_GRAY).withItalic(true)).first()
+                                }, false)
+                            } else it.source.sendFeedback({
+                                Text.of("Рейд не найден!")
+                                    .getWithStyle(Style.EMPTY.withColor(Colors.LIGHT_GRAY).withItalic(true)).first()
+                            }, false)
+                        }
+                        else it.source.sendFeedback({
+                            Text.of("Эту команду может выполнять только игрок!")
+                                .getWithStyle(Style.EMPTY.withColor(Colors.LIGHT_GRAY).withItalic(true)).first()
+                        }, false)
+                        0
+                    }
+            )
+        }
     }
 }
